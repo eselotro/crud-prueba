9dd084720df4e3b87738ff9a8894ecf3ec642576
eselotro <144515779+eselotro@users.noreply.github.com>
2025-01-07 17:11:12 -0300

    Update settings.py

--- stocklitecac/StockLite/settings.py
+++ stocklitecac/StockLite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-)77f)w_71#*4ky&bhpgty-f=fofge2ou%tam(h*a6=2+c3zl94
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['44.203.156.6', 'localhost']
+ALLOWED_HOSTS = ['54.211.220.123', 'localhost']
 
 
 # Application definition
