871dc673a076a9ff363e855fa91aa356e30b271d
eselotro <144515779+eselotro@users.noreply.github.com>
2025-01-06 22:52:53 -0300

    Update settings.py

--- stocklitecac/StockLite/settings.py
+++ stocklitecac/StockLite/settings.py
@@ -121,11 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Para archivos recopilados en producción
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
