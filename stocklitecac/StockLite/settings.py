cf6ee4a848e47f9c5f97a960294ac5fb0d9a8be6
eselotro <144515779+eselotro@users.noreply.github.com>
2025-01-07 22:42:24 -0300

    Update settings.py

--- stocklitecac/StockLite/settings.py
+++ stocklitecac/StockLite/settings.py
@@ -122,7 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = "/home/ubuntu/crud-prueba/static"
+STATIC_ROOT = "/home/ubuntu/crud-prueba/stocklitecac/static"
+
 
 
 
